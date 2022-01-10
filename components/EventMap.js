ef91da9b901a6833ca78831e9bdf34b5dcdfe9ba
lturpin <laurencejosephturpin@gmail.com>
2022-01-10 11:49:08 +0000

    viewport init

--- components/EventMap.js
+++ components/EventMap.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import { useState, useRef, useEffect } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
-// import DeckGL, { GeoJsonLayer } from 'deck.gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
@@ -9,8 +8,8 @@ const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
 const EventsMap = ({ evt }) => {
   const [loading, setLoading] = useState(true);
   const [viewport, setViewport] = useState({
-    latitude: 50.793278,
-    longitude: -0.002972,
+    latitude: 40.712772,
+    longitude: -73.93524,
     width: '100%',
     height: '500px',
     zoom: 12,
